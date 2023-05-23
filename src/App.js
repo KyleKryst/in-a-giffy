9153a2ad6eb57d7ff86c279faf011f2eafbafd47
KyleKryst <kryst.kyle@gmail.com>
2023-05-22 21:16:55 -0700

    All done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import SignUp from './pages/Signup';
+import Search from './pages/Search';
 
-export default App;
+const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  }); 
+
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="flex-column justify-center align-center min-100-vh bg-primary">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />}
+              />
+              <Route 
+                path="/Login" 
+                element={<Login />}
+              />
+              <Route 
+                path="/Logout" 
+                element={<Logout />}
+              />
+              <Route 
+                path="Signup"
+                element={<SignUp />}
+              />
+              <Route 
+                path="Search"
+                element={<Search />}
+              />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+  
+  export default App;
